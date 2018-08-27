3db39646dd841759bce82d5a0436e043661378d8
zhangxiujin <zxj669887072@gmail.com>
2018-08-27 16:01:07 +0800

    add   event

--- js-Advanced-programming.3/chapter-13-event/EventUtil.js
+++ js-Advanced-programming.3/chapter-13-event/EventUtil.js
@@ -1,6 +1,7 @@
 const EventUtil = {
+	//监听事件
 	addHandler: function(element, type, handler){
-		if (element.addEventListener){
+		if (element.addEventListener){		//alert('addEventListener')
 			element.addEventListener(type, handler, false);
 		} else if (element.attachEvent){
 			element.attachEvent("on" + type, handler);
@@ -8,6 +9,7 @@ const EventUtil = {
 			element["on" + type] = handler;
 		}
 	},
+	//移除事件
 	removeHandler: function(element, type, handler){
 		if (element.removeEventListener){
 			element.removeEventListener(type, handler, false);
@@ -17,19 +19,23 @@ const EventUtil = {
 			element["on" + type] = null;
 		}
 	},
+	//获取event对象
 	getEvent: function(event){
 		return event ? event : window.event;
 	},
+	//获取target对象
 	getTarget: function(event){
 		return event.target || event.srcElement;
 	},
+	//阻止默认行为
 	preventDefault: function(event){
 		if (event.preventDefault){
 			event.preventDefault();
 		} else {
-				event.returnValue = false;
+			event.returnValue = false;
 		}
 	},
+	//组织冒泡
 	stopPropagation: function(event){
 		if (event.stopPropagation){
 			event.stopPropagation();
@@ -37,6 +43,7 @@ const EventUtil = {
 			event.cancelBubble = true;
 		}
 	},
+	//获取相关元素
 	getRelatedTarget: function(event){
 		if (event.relatedTarget){
 			return event.relatedTarget;
@@ -48,7 +55,44 @@ const EventUtil = {
 			return null;
 		}
 	},
+	//获取鼠标按钮对应值
+	getButton: function(event){
+		if (document.implementation.hasFeature("MouseEvents", "2.0")){
+			return event.button;
+		} else {
+			switch(event.button){
+				case 0:
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+					return 0;
+				case 2:
+				case 6:
+					return 2;
+				case 4:
+					return 1;
+			}
+		}
+	},
+	//滚轮滚动时 detail 的属性值
+	getWheelDelta: function(event){
+		if (event.wheelDelta){
+			return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+		} else {
+			return -event.detail * 40;
+		}
+	},
+	//获取输入的字符编码
+	getCharCode: function(event){
+		if (typeof event.charCode == "number"){
+			return event.charCode;
+		} else {
+			return event.keyCode;
+		}
+	},
 };
+//节流防抖函数
 function throttle(method, context = null,event = null) {	//console.log(event)
 	clearTimeout(method.tId);
 	method.tId= setTimeout(function(){					//console.log(event)
